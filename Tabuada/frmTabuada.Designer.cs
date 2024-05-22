aab7eb55c5af2bf3412328b0a14d02239d6696f3
victor.ssosorio <victor.ssosorio@TIT0628220W10-1>
2024-05-21 21:06:38 -0300

    Adicionar arquivos de projeto.

---
+++ Tabuada/frmTabuada.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Tabuada
+{
+    partial class frmTabuada
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.nudNumero = new System.Windows.Forms.NumericUpDown();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumero)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Location = new System.Drawing.Point(12, 18);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(148, 13);
+            this.lblNumero.TabIndex = 0;
+            this.lblNumero.Text = "Entre com o número desejado";
+            // 
+            // nudNumero
+            // 
+            this.nudNumero.Location = new System.Drawing.Point(171, 13);
+            this.nudNumero.Name = "nudNumero";
+            this.nudNumero.Size = new System.Drawing.Size(44, 20);
+            this.nudNumero.TabIndex = 1;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(15, 39);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(200, 144);
+            this.txtResultado.TabIndex = 2;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Image = global::Tabuada.Properties.Resources.eraser;
+            this.btnLimpar.Location = new System.Drawing.Point(15, 189);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(97, 53);
+            this.btnLimpar.TabIndex = 3;
+            this.btnLimpar.Text = "&Limpar Campos";
+            this.btnLimpar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnLimpar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Image = global::Tabuada.Properties.Resources.calculator;
+            this.btnCalcular.Location = new System.Drawing.Point(118, 189);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(97, 53);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "&Calcular";
+            this.btnCalcular.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCalcular.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // frmTabuada
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(227, 252);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.nudNumero);
+            this.Controls.Add(this.lblNumero);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmTabuada";
+            this.Text = "Tabuada";
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumero)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.NumericUpDown nudNumero;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Tabuada/frmTabuada.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tabuada
+{
+    public partial class frmTabuada : Form
+    {
+        public frmTabuada()
+        {
+            InitializeComponent();
+        }
+
+        private void LimparCampos()
+        {
+            nudNumero.Value = 0;
+            txtResultado.Clear();
+            nudNumero.Focus();
+        }
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+        }
+        private string Calcular(int numero)
+        {
+            string tabuada = string.Empty;
+            int contador = 0;
+            int resultado = 0;
+
+            for (contador =1; contador <= 10; contador++)
+            {
+                resultado = numero * contador;
+                tabuada += string.Format("{0} X {1} = {2} \r\n",
+                    numero,contador,resultado);
+                //tabuada += $"{numero} X {contador} = {resultado} \r\n";
+            }
+
+            return tabuada;
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = Calcular((int)nudNumero.Value);
+        }
+    }
+}
